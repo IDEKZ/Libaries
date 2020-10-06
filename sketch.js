88251cddfbb8c101161cacebfac14e430eb4216b
Rasail <70968150+Rasail@users.noreply.github.com>
2020-10-06 18:52:21 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,20 +1,34 @@
-var fixedRect, movingRect;
-
+const Engine=Matter. Engine;
+const World= Matter. World;
+const Bodies=Matter. Bodies; 
+var engine, world;
+var ground
+var ball
 function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(400, 100, 50, 50);
-  fixedRect.shapeColor = "white";
-  fixedRect.debug = true;
-  movingRect = createSprite(400, 800,50,50);
-  movingRect.shapeColor = "white";
-  movingRect.debug = true;
-
-  movingRect.velocityY = -5;
-  fixedRect.velocityY = +5;
+createCanvas(800,800);
+var static={
+isStatic:true
+}
+var notStatic={
+restitution:2
+}
+console.log(ball);
+engine=Engine.create()
+world=engine.world;
+ground=Bodies.rectangle(400,700,800,50, static)
+ball=Bodies.circle(200,400,30,notStatic)
+World.add(world,ground)
+World.add(world,ball)
 }
 
 function draw() {
+  
   background(0,0,0);  
- bounce(movingRect, fixedRect)
+ Engine.update(engine); 
+
+  rectMode(CENTER); 
+  rect (ground.position.x,ground.position.y,800,50);
+  ellipseMode(RADIUS);
+  ellipse(ball.position.x,ball.position.y,30,30);
   drawSprites();
 }
